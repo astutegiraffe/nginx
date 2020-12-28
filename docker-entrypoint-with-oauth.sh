ff80538fb7bb09138b06e3b1e387206742566aa5
Srinath Ramanujan <srinath.ramanujan@icloud.com>
2020-12-28 06:25:53 +0530

    Fixing incorrect binary name for oauth2-proxy

--- docker-entrypoint-with-oauth.sh
+++ docker-entrypoint-with-oauth.sh
@@ -8,9 +8,9 @@ NL=$'\n'
 graceful_exit() {
 	echo "Exiting docker-entrypoint-with-oauth.sh gracefuly due to signal $1"
 	kill $1 $NGINX_PID || wait $NGINX_PID
-	echo "Nginx exited, now signalling oauth2proxy pids."
+	echo "Nginx exited, now signalling oauth2-proxy pids."
 	kill -TERM $OAUTH2PROXY_PIDS || wait $OAUTH2PROXY_PIDS
-	echo "oauth2proxy pids exited."
+	echo "oauth2-proxy pids exited."
 	exit 0
 }
 
@@ -23,8 +23,8 @@ NGINX_PID=$!
 
 if [ -n "$OAUTH2PROXY_VARS" ]; then
 	echo "$OAUTH2PROXY_VARS" | while read -r line; do
-		echo "Starting oauth2proxy with ${line}"
-		/oauth2proxy $line &
+		echo "Starting oauth2-proxy with ${line}"
+		/oauth2-proxy $line &
 		OAUTH2PROXY_PIDS="${OAUTH2PROXY_PIDS}${NL}$!"
 	done
 fi
